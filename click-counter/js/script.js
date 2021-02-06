76ce406d4c283de47b4f4efafd1eaefd2e238d3b
inxaneninja <inxaneninja@gmail.com>
2021-02-06 15:05:32 +0100

    Updated website, added click counter

---
+++ click-counter/js/script.js
@@ -0,0 +1,13 @@
+let clicks = 0;
+let clickDisplay = document.querySelector("#clicks");
+
+let clickCallback = (ev) => {
+	ev.preventDefault();
+	clicks++;
+
+	clickDisplay.textContent = clicks.toString();
+	return false;
+}
+
+document.addEventListener("click", clickCallback);
+document.addEventListener("contextmenu", clickCallback);
